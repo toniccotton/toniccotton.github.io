49fc69f5ffb6669ef74f643e6176d74201ffbac4
Brandon Leigh-Bennett <bleighbennett@benbria.ca>
2015-11-25 19:40:09 -0500

    Link html to javascript

--- javascript/fish.js
+++ javascript/fish.js
@@ -1,4 +1,11 @@
-background(89, 216, 255);
+var start = function(processing) { with (processing) {
+//
+// BEGIN PROCESSING ENVIRONMENT
+//
+
+var setup = function () {
+    size(400,400);
+}
 
 var drawFish = function (x, y, length, height, color2, tailWidth, tailColor, eyeSize) {
 
@@ -6,8 +13,8 @@ var drawFish = function (x, y, length, height, color2, tailWidth, tailColor, eye
     var centerY = y;
     var bodyLength = length;
     var bodyHeight = height;
-    
-        
+
+
     noStroke();
     fill(color2);
     // body
@@ -15,8 +22,8 @@ var drawFish = function (x, y, length, height, color2, tailWidth, tailColor, eye
     // tail
     fill(tailColor);
     //tailColor
-    
-    
+
+
     var tailHeight = bodyHeight/2;
     triangle(centerX-bodyLength/2, centerY,
              centerX-bodyLength/2-tailWidth, centerY-tailHeight,
@@ -26,8 +33,17 @@ var drawFish = function (x, y, length, height, color2, tailWidth, tailColor, eye
     ellipse(centerX+bodyLength/4, centerY, eyeSize, eyeSize);
 };
 
-drawFish(270, 200, 80, 40, color(224, 38, 112), 20, color(217, 162, 21), 10);
-drawFish(200, 300, 160, 40, color(120, 141, 204), 60, color(245, 22, 245), 25);
-drawFish(240, 80, 230, 60, color(49, 176, 42), 80, color(9, 0, 255), 40);
-         
-         
+draw = function () {
+    background(89, 216, 255);
+    drawFish(270, 200, 80, 40, color(224, 38, 112), 20, color(217, 162, 21), 10);
+    drawFish(200, 300, 160, 40, color(120, 141, 204), 60, color(245, 22, 245), 25);
+    drawFish(240, 80, 230, 60, color(49, 176, 42), 80, color(9, 0, 255), 40);
+}
+
+
+//
+// END PROCESSING ENVIRONMENT
+//
+}};
+var canvas = document.getElementById('canvas1');
+new Processing(canvas, start);
