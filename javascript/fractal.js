1174901eaebfea2aa5464b5d9bcf5bcf70408ed0
Brandon Leigh-Bennett <bleighbennett@benbria.ca>
2015-12-08 19:51:28 -0500

    add fractal files

---
+++ javascript/fractal.js
@@ -0,0 +1,17 @@
+var start = function(processing) { with (processing) {
+//
+// BEGIN PROCESSING ENVIRONMENT
+//
+
+
+draw = function () {
+
+}
+
+
+//
+// END PROCESSING ENVIRONMENT
+//
+}};
+var canvas = document.getElementById('canvas1');
+new Processing(canvas, start);
