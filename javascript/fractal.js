6b0d0889f618e14e856ea8f2ab75822f189dbc34
toniccotton <toniccotton@gmail.com>
2015-12-08 20:31:07 -0500

    remove background, change line colour depending on mouseX

--- javascript/fractal.js
+++ javascript/fractal.js
@@ -2,14 +2,22 @@ var start = function(processing) { with (processing) {
 //
 // BEGIN PROCESSING ENVIRONMENT
 //
-size(400, 400);
+size(800, 400);
 
 draw = function () {
-	background(20, 133, 100);
-
+	//background(20, 133, 100);
+	stroke(mouseX / 2, 50, 50);
 	line(200, 200, mouseX, mouseY);
 
 };
+
+mouseClicked = function () {
+ 
+
+
+	alert("it works");
+
+}
 
 
 //
