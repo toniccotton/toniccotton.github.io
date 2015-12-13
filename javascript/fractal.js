34b7908f7c3ecbd6bcac2c8e4b1bf7e002266d7b
toniccotton <toniccotton@gmail.com>
2015-12-13 17:46:14 -0500

    change background color every time mouse is clicked

--- javascript/fractal.js
+++ javascript/fractal.js
@@ -4,19 +4,29 @@ var start = function(processing) { with (processing) {
 //
 size(800, 400);
 
+var clickCount = 1;
+var backgroundColors = [color(255, 20, 147), color(20, 70, 150), color(10, 10, 3)];
+background(backgroundColors[0])
+
+mouseClicked = function () {
+	background(backgroundColors[clickCount])
+		
+	clickCount = clickCount + 1;
+
+	if (clickCount > 2)	{
+		clickCount = 0;
+	}	
+
+};
+
 draw = function () {
-	//background(20, 133, 100);
+	
 	//noStroke();
 	fill(mouseX / 4, mouseY / 2, (mouseX / mouseY) * 100, 125);
 	ellipse(mouseX, mouseY, 40, 40);
 
 };
 
-mouseClicked = function () {
-
- 	background(255, 20, 147);
-
-}
 
 
 //
